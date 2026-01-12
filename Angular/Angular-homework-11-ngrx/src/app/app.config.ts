025e99988160572dcd2a87063b1f61bf17441a95
NikaMaghaldadze <nikamaghaldadze13@gmail.com>
2026-01-12 23:18:02 +0400

    initializing application
    adding app shell
    adding routing blueprint

---
+++ Angular/Angular-homework-11-ngrx/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    provideStore(),
+    provideEffects(),
+  ]
+};
---
+++ Angular/Angular-homework-11-ngrx/src/app/features/quiz/quiz.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { QuizShellComponent } from './quiz-shell.component';
+
+export const QUIZ_ROUTES: Routes = [
+  { path: '', component: QuizShellComponent },
+];
